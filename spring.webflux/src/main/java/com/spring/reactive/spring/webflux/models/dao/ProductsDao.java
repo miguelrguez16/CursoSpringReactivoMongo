754486ec9d977bcedbc730639f9fe614a56826b4
Miguel Rguez <uo263494@uniovi.es>
2022-07-25 19:46:14 +0200

    feat: add entity and repository reactive Mongo

---
+++ spring.webflux/src/main/java/com/spring/reactive/spring/webflux/models/dao/ProductsDao.java
@@ -0,0 +1,9 @@
+package com.spring.reactive.spring.webflux.models.dao;
+
+import com.spring.reactive.spring.webflux.models.documents.Product;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+
+public interface ProductsDao extends ReactiveMongoRepository<Product, String> {
+
+}
---
+++ spring.webflux/src/main/java/com/spring/reactive/spring/webflux/models/documents/Product.java
@@ -0,0 +1,62 @@
+package com.spring.reactive.spring.webflux.models.documents;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+@Document(collection = "products")
+public class Product {
+    @Id
+    private String id;
+
+    private String name;
+    private Double price;
+    private Date createAt;
+
+    public String getId() {
+        return id;
+    }
+
+    public Product setId(String id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Product setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public Product setPrice(Double price) {
+        this.price = price;
+        return this;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public Product setCreateAt(Date createAt) {
+        this.createAt = createAt;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", createAt=" + createAt +
+                '}';
+    }
+}
