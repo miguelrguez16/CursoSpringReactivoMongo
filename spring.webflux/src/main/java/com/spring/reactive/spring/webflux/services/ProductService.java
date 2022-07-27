4a1fb3a18a99e3439250771a27615c3775aab5a5
Miguel Rguez <uo263494@uniovi.es>
2022-07-27 12:36:50 +0200

    feat: service implementation

---
+++ spring.webflux/src/main/java/com/spring/reactive/spring/webflux/services/ProductService.java
@@ -0,0 +1,17 @@
+package com.spring.reactive.spring.webflux.services;
+
+import com.spring.reactive.spring.webflux.models.documents.Product;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface ProductService {
+
+    Flux<Product> findAll();
+    Mono<Product> findById(String id);
+
+    Mono<Product> save(Product product);
+
+    Mono<Void> delete(Product product);
+
+
+}
