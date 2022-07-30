d9920573e550c086c7d048d749883a69e91e1213
Miguel Rguez <uo263494@uniovi.es>
2022-07-30 22:28:47 +0200

    Initial Spring Api Rest

---
+++ springApiRest/src/main/java/com/example/springApiRest/SpringApiRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springApiRest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiRestApplication.class, args);
+	}
+
+}
