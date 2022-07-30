e72edd6121ca8e0b0ac96d9a2f40b24b14e0c910
Miguel Rguez <uo263494@uniovi.es>
2022-07-30 22:28:47 +0200

    Initial Spring Api Rest

---
+++ springApiRest/src/test/java/com/example/springApiRest/SpringApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springApiRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
