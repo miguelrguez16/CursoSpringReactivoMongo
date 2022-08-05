5a109b93a601a9f7c488c586c22956f254541f17
Miguel Rguez <uo263494@uniovi.es>
2022-08-05 19:12:43 +0200

    feat: functional endpoints save edit

--- springApiRest/src/main/java/com/example/springapirest/RouterFunctionConfig.java
+++ springApiRest/src/main/java/com/example/springapirest/RouterFunctionConfig.java
@@ -7,16 +7,20 @@ import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
-import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
 
 @Configuration
 public class RouterFunctionConfig {
+    private static final String DEFAULT_URI ="/api/v2/products";
 
     @Bean
     public RouterFunction<ServerResponse> routes(ProductHandler productHandler) {
         return RouterFunctions
-                .route(GET("/api/v2/products").or(GET("/api/v3/products")),productHandler::list)
-                .andRoute(GET("/api/v2/products/{id}"),productHandler::detail);
+                .route(GET(DEFAULT_URI).or(GET("/api/v3/products")),productHandler::list)
+                .andRoute(GET(DEFAULT_URI.concat("/{id}")),productHandler::detail)
+                .andRoute(POST(DEFAULT_URI),productHandler::save)
+                .andRoute(PUT(DEFAULT_URI),productHandler::edit);
+
     }   // indicate response type is JSON with .and(contentType(MediaType.APPLICATION_JSON))
         // the request must have indicated content-type: json
 }
--- springApiRest/src/main/java/com/example/springapirest/handler/ProductHandler.java
+++ springApiRest/src/main/java/com/example/springapirest/handler/ProductHandler.java
@@ -10,24 +10,69 @@ import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
 
+import java.net.URI;
+import java.util.Date;
+
 @Component
 public class ProductHandler {
     @Autowired
     private ProductService productService;
 
-    public Mono<ServerResponse> list(ServerRequest serverRequest){
+    /**
+     * @param serverRequest request fr
+     * @return list of products
+     */
+    public Mono<ServerResponse> list(ServerRequest serverRequest) {
         return ServerResponse.ok()
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(productService.findAllProducts(), Product.class);
     }
-    public Mono<ServerResponse> detail(ServerRequest serverRequest){
+
+    /**
+     * @param serverRequest with the id of the product to see
+     * @return
+     */
+    public Mono<ServerResponse> detail(ServerRequest serverRequest) {
         String idRequest = serverRequest.pathVariable("id");
         return productService.findProductById(idRequest)
                 .flatMap(product -> ServerResponse.ok()
-                                .contentType(MediaType.APPLICATION_JSON)
-                                .body(BodyInserters.fromValue(product)))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .body(BodyInserters.fromValue(product)))
                 .switchIfEmpty(ServerResponse.notFound().build());
     }
 
+    /**
+     * Save a product from a post
+     *
+     * @param serverRequest with the product in Json format
+     * @return the uri to find the product and the product
+     */
+    public Mono<ServerResponse> save(ServerRequest serverRequest) {
+        Mono<Product> productMono = serverRequest.bodyToMono(Product.class);
+        return productMono.flatMap(product -> {
+                    if (product.getCreateAt() == null) product.setCreateAt(new Date());
+                    return productService.saveProduct(product);
+                })
+                .flatMap(product ->
+                        ServerResponse.created(URI.create("/api/v2/products/".concat(product.getId())))
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .body(BodyInserters.fromValue(product)));
+    }
+
+    public Mono<ServerResponse> edit(ServerRequest serverRequest) {
+        String idRequest = serverRequest.pathVariable("id");
+        Mono<Product> productMono = serverRequest.bodyToMono(Product.class);
+
+        Mono<Product> productMonoDb = productService.findProductById(idRequest);
+
+        return productMonoDb.zipWith(productMono, (db, req) -> {
+            db.setName(req.getName())
+                    .setPrice(req.getPrice())
+                    .setCategory(req.getCategory());
+            return db;
+        }).flatMap(product -> ServerResponse.created(URI.create("/api/v2/products/".concat(product.getId())))
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(product)));
+    }
 
 }
