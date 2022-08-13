7c8547e5e453c333f5514cfbaa92d3b3693cc392
Miguel Rguez <uo263494@uniovi.es>
2022-08-13 17:40:23 +0200

    feat: Handlers update save SaveAndUpload

--- springApiRest/src/main/java/com/example/springapirest/RouterFunctionConfig.java
+++ springApiRest/src/main/java/com/example/springapirest/RouterFunctionConfig.java
@@ -11,16 +11,20 @@ import static org.springframework.web.reactive.function.server.RequestPredicates
 
 @Configuration
 public class RouterFunctionConfig {
-    private static final String DEFAULT_URI ="/api/v2/products";
+    private static final String DEFAULT_URI = "/api/v2/products";
+    private static final String DEFAULT_URI_ID = "/api/v2/products/{id}";
 
     @Bean
     public RouterFunction<ServerResponse> routes(ProductHandler productHandler) {
         return RouterFunctions
-                .route(GET(DEFAULT_URI).or(GET("/api/v3/products")),productHandler::list)
-                .andRoute(GET(DEFAULT_URI.concat("/{id}")),productHandler::detail)
-                .andRoute(POST(DEFAULT_URI),productHandler::save)
-                .andRoute(PUT(DEFAULT_URI),productHandler::edit);
+                .route(GET(DEFAULT_URI).or(GET("/api/v3/products")), productHandler::list)
+                .andRoute(GET(DEFAULT_URI_ID), productHandler::detail)
+                .andRoute(POST(DEFAULT_URI), productHandler::save)
+                .andRoute(PUT(DEFAULT_URI), productHandler::edit)
+                .andRoute(DELETE(DEFAULT_URI_ID), productHandler::delete)
+                .andRoute(POST(DEFAULT_URI_ID), productHandler::upload)
+                .andRoute(POST(DEFAULT_URI), productHandler::saveAndUpload);
 
     }   // indicate response type is JSON with .and(contentType(MediaType.APPLICATION_JSON))
-        // the request must have indicated content-type: json
+    // the request must have indicated content-type: json
 }
