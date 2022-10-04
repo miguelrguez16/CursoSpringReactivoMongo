2dd5faee7f0a62f980bf7d7fbf3dd75fd88a915d
Miguel Rguez <uo263494@uniovi.es>
2022-10-04 22:35:13 +0200

    feat: test con StepVerifier

---
+++ eureka-spring/eureka-spring/src/main/java/es/java/eureka/eurekaspring/EurekaSpringApplication.java
@@ -0,0 +1,13 @@
+package es.java.eureka.eurekaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaSpringApplication.class, args);
+	}
+
+}
